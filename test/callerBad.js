cb2f9f6f7fd2315a25c3bc76c370c692201034b5
Xavier Lepretre <xavier.lepretre@gmail.com>
2017-02-20 15:26:07 +0100

    Indentation.

--- test/callerBad.js
+++ test/callerBad.js
@@ -3,87 +3,87 @@ Extensions.init(web3, assert);
 
 contract('Caller bad calls', function(accounts) {
 
-  it("calling bad version does not return expected values", function() {
+    it("calling bad version does not return expected values", function() {
 
-    var caller = Caller.deployed();
-    var tool = Tool.deployed();
+        var caller = Caller.deployed();
+        var tool = Tool.deployed();
 
-    return tool.number()
-      // We can call a public field
-      .then(function (number) {
-        assert.equal(number, 0, "number should start at 0");
-        return tool.setNumber(2, { from: accounts[0] });
-      })
-      .then(function (tx) {
-        return web3.eth.getTransactionReceiptMined(tx);
-        // Waiting for it to be mined
-      })
-      .then(function (receipt) {
-        return tool.number();
-      })
-      .then(function (number) {
-        assert.equal(number, 2, "number should now be 2");
-        return tool.numberIsEven.call();
-        // Because it is a .call, we get the return value
-      })
-      .then(function (isEven) {
-        assert.isTrue(isEven, "number should be said even");
-        return caller.saved();
-      })
-      .then(function (saved) {
-        assert.equal(saved, 42, "saved should start at 42");
-        return caller.getNumberIfEvenBad.call({ from: accounts[0] });
-        // Because it is a .call, we get the return value
-        // But there is an inner call to tool.numberIsEven()
-      })
-      .then(function (saved) {
-        assert.equal(saved, 2, "should get the number because it is even");
-        return caller.getNumberIfEvenBad({ from: accounts[0] });
-        // Because it is a direct call, we get the txn
-      })
-      .then(function (tx) {
-        return web3.eth.getTransactionReceiptMined(tx);
-      })
-      .then(function (receipt) {
-        return caller.saved();
-      })
-      .then(function (saved) {
-        assert.equal(saved, 2, "should have been updated to the number too");
-        return tool.setNumber(3, { from: accounts[0] });
-      })
-      .then(function (tx) {
-        return web3.eth.getTransactionReceiptMined(tx);
-      })
-      .then(function (receipt) {
-        return tool.number();
-      })
-      .then(function (number) {
-        assert.equal(number, 3, "number should now be 3");
-        return tool.numberIsEven.call({ from: accounts[0] });
-      })
-      .then(function (isEven) {
-        assert.isFalse(isEven, "number should be said odd now");
-        return caller.getNumberIfEvenBad.call({ from: accounts[0] });
-        // Because it is a .call, we get the return value
-      })
-      .then(function (saved) {
-        // --> That's where it is weird!!
-        // We get the unchanged saved value
-        assert.equal(saved.valueOf(), 2, "should still get 2 because it is the unchanged saved value");
-        return caller.getNumberIfEvenBad({ from: accounts[0] });
-        // Because it is a direct call, we get the txn
-      })
-      .then(function (tx) {
-        return web3.eth.getTransactionReceiptMined(tx);
-      })
-      .then(function (receipt) {
-        return caller.saved();
-      })
-      .then(function (saved) {
-        // But when checking after a transaction, the proper value was saved...
-        assert.equal(saved, 0, "should have been updated to 0 too");
-      });
+        return tool.number()
+            // We can call a public field
+            .then(function (number) {
+                assert.equal(number, 0, "number should start at 0");
+                return tool.setNumber(2, { from: accounts[0] });
+            })
+            .then(function (tx) {
+                return web3.eth.getTransactionReceiptMined(tx);
+                // Waiting for it to be mined
+            })
+            .then(function (receipt) {
+                return tool.number();
+            })
+            .then(function (number) {
+                assert.equal(number, 2, "number should now be 2");
+                return tool.numberIsEven.call();
+                // Because it is a .call, we get the return value
+            })
+            .then(function (isEven) {
+                assert.isTrue(isEven, "number should be said even");
+                return caller.saved();
+            })
+            .then(function (saved) {
+                assert.equal(saved, 42, "saved should start at 42");
+                return caller.getNumberIfEvenBad.call({ from: accounts[0] });
+                // Because it is a .call, we get the return value
+                // But there is an inner call to tool.numberIsEven()
+            })
+            .then(function (saved) {
+                assert.equal(saved, 2, "should get the number because it is even");
+                return caller.getNumberIfEvenBad({ from: accounts[0] });
+                // Because it is a direct call, we get the txn
+            })
+            .then(function (tx) {
+                return web3.eth.getTransactionReceiptMined(tx);
+            })
+            .then(function (receipt) {
+                return caller.saved();
+            })
+            .then(function (saved) {
+                assert.equal(saved, 2, "should have been updated to the number too");
+                return tool.setNumber(3, { from: accounts[0] });
+            })
+            .then(function (tx) {
+                return web3.eth.getTransactionReceiptMined(tx);
+            })
+            .then(function (receipt) {
+                return tool.number();
+            })
+            .then(function (number) {
+                assert.equal(number, 3, "number should now be 3");
+                return tool.numberIsEven.call({ from: accounts[0] });
+            })
+            .then(function (isEven) {
+                assert.isFalse(isEven, "number should be said odd now");
+                return caller.getNumberIfEvenBad.call({ from: accounts[0] });
+                // Because it is a .call, we get the return value
+            })
+            .then(function (saved) {
+                // --> That's where it is weird!!
+                // We get the unchanged saved value
+                assert.equal(saved.valueOf(), 2, "should still get 2 because it is the unchanged saved value");
+                return caller.getNumberIfEvenBad({ from: accounts[0] });
+                // Because it is a direct call, we get the txn
+            })
+            .then(function (tx) {
+                return web3.eth.getTransactionReceiptMined(tx);
+            })
+            .then(function (receipt) {
+                return caller.saved();
+            })
+            .then(function (saved) {
+                // But when checking after a transaction, the proper value was saved...
+                assert.equal(saved, 0, "should have been updated to 0 too");
+            });
 
-  });
+    });
 
 });
--- test/callerGood.js
+++ test/callerGood.js
@@ -3,84 +3,84 @@ Extensions.init(web3, assert);
 
 contract('Caller bad calls', function(accounts) {
 
-  it("calling good version should return correct values", function() {
+    it("calling good version should return correct values", function() {
 
-    var caller = Caller.deployed();
-    var tool = Tool.deployed();
+        var caller = Caller.deployed();
+        var tool = Tool.deployed();
 
-    return tool.number()
-      // We can call a public field
-      .then(function (number) {
-        assert.equal(number, 0, "number should start at 0");
-        return tool.setNumber(2, { from: accounts[0] });
-      })
-      .then(function (tx) {
-        return web3.eth.getTransactionReceiptMined(tx);
-        // Waiting for it to be mined
-      })
-      .then(function (receipt) {
-        return tool.number();
-      })
-      .then(function (number) {
-        assert.equal(number, 2, "number should now be 2");
-        return tool.numberIsEven.call();
-        // Because it is a .call, we get the return value
-      })
-      .then(function (isEven) {
-        assert.isTrue(isEven, "number should be said even");
-        return caller.saved();
-      })
-      .then(function (saved) {
-        assert.equal(saved, 42, "saved should start at 42");
-        return caller.getNumberIfEvenGood.call({ from: accounts[0] });
-        // Because it is a .call, we get the return value
-        // But there is an inner call to tool.numberIsEven()
-      })
-      .then(function (saved) {
-        assert.equal(saved, 2, "should get the number because it is even");
-        return caller.getNumberIfEvenGood({ from: accounts[0] });
-        // Because it is a direct call, we get the txn
-      })
-      .then(function (tx) {
-        return web3.eth.getTransactionReceiptMined(tx);
-      })
-      .then(function (receipt) {
-        return caller.saved();
-      })
-      .then(function (saved) {
-        assert.equal(saved, 2, "should have been updated to the number too");
-        return tool.setNumber(3, { from: accounts[0] });
-      })
-      .then(function (tx) {
-        return web3.eth.getTransactionReceiptMined(tx);
-      })
-      .then(function (receipt) {
-        return tool.number();
-      })
-      .then(function (number) {
-        assert.equal(number, 3, "number should now be 3");
-        return tool.numberIsEven.call({ from: accounts[0] });
-      })
-      .then(function (isEven) {
-        assert.isFalse(isEven, "number should be said odd now");
-        return caller.getNumberIfEvenGood.call({ from: accounts[0] });
-        // Because it is a .call, we get the return value
-      })
-      .then(function (saved) {
-        assert.equal(saved.valueOf(), 0, "should get 0 because it is odd");
-        return caller.getNumberIfEvenGood({ from: accounts[0] });
-        // Because it is a direct call, we get the txn
-      })
-      .then(function (tx) {
-        return web3.eth.getTransactionReceiptMined(tx);
-      })
-      .then(function (receipt) {
-        return caller.saved();
-      })
-      .then(function (saved) {
-        assert.equal(saved, 0, "should have been updated to 0 too");
-      });
+        return tool.number()
+            // We can call a public field
+            .then(function (number) {
+                assert.equal(number, 0, "number should start at 0");
+                return tool.setNumber(2, { from: accounts[0] });
+            })
+            .then(function (tx) {
+                return web3.eth.getTransactionReceiptMined(tx);
+                // Waiting for it to be mined
+            })
+            .then(function (receipt) {
+                return tool.number();
+            })
+            .then(function (number) {
+                assert.equal(number, 2, "number should now be 2");
+                return tool.numberIsEven.call();
+                // Because it is a .call, we get the return value
+            })
+            .then(function (isEven) {
+                assert.isTrue(isEven, "number should be said even");
+                return caller.saved();
+            })
+            .then(function (saved) {
+                assert.equal(saved, 42, "saved should start at 42");
+                return caller.getNumberIfEvenGood.call({ from: accounts[0] });
+                // Because it is a .call, we get the return value
+                // But there is an inner call to tool.numberIsEven()
+            })
+            .then(function (saved) {
+                assert.equal(saved, 2, "should get the number because it is even");
+                return caller.getNumberIfEvenGood({ from: accounts[0] });
+                // Because it is a direct call, we get the txn
+            })
+            .then(function (tx) {
+                return web3.eth.getTransactionReceiptMined(tx);
+            })
+            .then(function (receipt) {
+                return caller.saved();
+            })
+            .then(function (saved) {
+                assert.equal(saved, 2, "should have been updated to the number too");
+                return tool.setNumber(3, { from: accounts[0] });
+            })
+            .then(function (tx) {
+                return web3.eth.getTransactionReceiptMined(tx);
+            })
+            .then(function (receipt) {
+                return tool.number();
+            })
+            .then(function (number) {
+                assert.equal(number, 3, "number should now be 3");
+                return tool.numberIsEven.call({ from: accounts[0] });
+            })
+            .then(function (isEven) {
+                assert.isFalse(isEven, "number should be said odd now");
+                return caller.getNumberIfEvenGood.call({ from: accounts[0] });
+                // Because it is a .call, we get the return value
+            })
+            .then(function (saved) {
+                assert.equal(saved.valueOf(), 0, "should get 0 because it is odd");
+                return caller.getNumberIfEvenGood({ from: accounts[0] });
+                // Because it is a direct call, we get the txn
+            })
+            .then(function (tx) {
+                return web3.eth.getTransactionReceiptMined(tx);
+            })
+            .then(function (receipt) {
+                return caller.saved();
+            })
+            .then(function (saved) {
+                assert.equal(saved, 0, "should have been updated to 0 too");
+            });
 
-  });
+    });
 
 });
