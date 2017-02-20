cd8fd502775fd55230aa018e4689a6ed8670008c
Xavier Lepretre <xavier.lepretre@gmail.com>
2017-02-20 15:25:16 +0100

    Make use of extensions.

--- test/callerBad.js
+++ test/callerBad.js
@@ -1,25 +1,5 @@
-web3.eth.getTransactionReceiptMined = function (txnHash, interval) {
-  var transactionReceiptAsync;
-  interval |= 500;
-  transactionReceiptAsync = function(txnHash, resolve, reject) {
-    try {
-      var receipt = web3.eth.getTransactionReceipt(txnHash);
-      if (receipt == null) {
-        setTimeout(function () {
-          transactionReceiptAsync(txnHash, resolve, reject);
-        }, 500);
-      } else {
-        resolve(receipt);
-      }
-    } catch(e) {
-      reject(e);
-    }
-  };
-
-  return new Promise(function (resolve, reject) {
-    transactionReceiptAsync(txnHash, resolve, reject);
-  });
-};
+Extensions = require("../utils/extensions.js");
+Extensions.init(web3, assert);
 
 contract('Caller bad calls', function(accounts) {
 
