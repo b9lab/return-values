94cfa29ca04abb3bbc6e920d8368cc0903bcfcaf
Xavier Lepretre <xavier.lepretre@gmail.com>
2017-02-20 15:32:45 +0100

    Arrow notation.

--- test/callerBad.js
+++ test/callerBad.js
@@ -10,76 +10,59 @@ contract('Caller bad calls', function(accounts) {
 
         return tool.number()
             // We can call a public field
-            .then(function (number) {
+            .then(number => {
                 assert.equal(number, 0, "number should start at 0");
                 return tool.setNumber(2, { from: accounts[0] });
             })
-            .then(function (tx) {
-                return web3.eth.getTransactionReceiptMined(tx);
-                // Waiting for it to be mined
-            })
-            .then(function (receipt) {
-                return tool.number();
-            })
-            .then(function (number) {
+            .then(web3.eth.getTransactionReceiptMined)
+            .then(receipt => tool.number())
+            .then(number => {
                 assert.equal(number, 2, "number should now be 2");
                 return tool.numberIsEven.call();
                 // Because it is a .call, we get the return value
             })
-            .then(function (isEven) {
+            .then(isEven => {
                 assert.isTrue(isEven, "number should be said even");
                 return caller.saved();
             })
-            .then(function (saved) {
+            .then(saved => {
                 assert.equal(saved, 42, "saved should start at 42");
                 return caller.getNumberIfEvenBad.call({ from: accounts[0] });
                 // Because it is a .call, we get the return value
                 // But there is an inner call to tool.numberIsEven()
             })
-            .then(function (saved) {
+            .then(saved => {
                 assert.equal(saved, 2, "should get the number because it is even");
                 return caller.getNumberIfEvenBad({ from: accounts[0] });
                 // Because it is a direct call, we get the txn
             })
-            .then(function (tx) {
-                return web3.eth.getTransactionReceiptMined(tx);
-            })
-            .then(function (receipt) {
-                return caller.saved();
-            })
-            .then(function (saved) {
+            .then(web3.eth.getTransactionReceiptMined)
+            .then(receipt => caller.saved())
+            .then(saved => {
                 assert.equal(saved, 2, "should have been updated to the number too");
                 return tool.setNumber(3, { from: accounts[0] });
             })
-            .then(function (tx) {
-                return web3.eth.getTransactionReceiptMined(tx);
-            })
-            .then(function (receipt) {
-                return tool.number();
-            })
-            .then(function (number) {
+            .then(web3.eth.getTransactionReceiptMined)
+            .then(receipt => tool.number())
+            .then(number => {
                 assert.equal(number, 3, "number should now be 3");
                 return tool.numberIsEven.call({ from: accounts[0] });
             })
-            .then(function (isEven) {
+            .then(isEven => {
                 assert.isFalse(isEven, "number should be said odd now");
                 return caller.getNumberIfEvenBad.call({ from: accounts[0] });
                 // Because it is a .call, we get the return value
             })
-            .then(function (saved) {
+            .then(saved => {
                 // --> That's where it is weird!!
                 // We get the unchanged saved value
                 assert.equal(saved.valueOf(), 2, "should still get 2 because it is the unchanged saved value");
                 return caller.getNumberIfEvenBad({ from: accounts[0] });
                 // Because it is a direct call, we get the txn
             })
-            .then(function (tx) {
-                return web3.eth.getTransactionReceiptMined(tx);
-            })
-            .then(function (receipt) {
-                return caller.saved();
-            })
-            .then(function (saved) {
+            .then(web3.eth.getTransactionReceiptMined)
+            .then(receipt => caller.saved())
+            .then(saved => {
                 // But when checking after a transaction, the proper value was saved...
                 assert.equal(saved, 0, "should have been updated to 0 too");
             });
